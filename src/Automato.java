c27ebc1678d64ca4a13d00f9d63955a554d00439
MrDobler <arthur.dobler@gmail.com>
2017-10-12 18:46:41 -0300

    Automato Criado

---
+++ src/Automato.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+import java.lang.*;
+
+public class Automato  {
+	
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+		String resposta;
+		
+		while(true){
+			Scanner input = new Scanner(System.in);
+			System.out.println("Informa a palavra: ");
+			String palavraOriginal = input.nextLine();
+			
+			String palavraLower = palavraOriginal.toLowerCase();
+			char[] palavra = new char[palavraOriginal.length()-1];
+			palavra = palavraLower.toCharArray();
+			int estado = 0;
+			
+			Transicao transicao = new Transicao();
+			
+			int resultado = transicao.funTransicao(palavra, estado);
+			
+			
+			if(resultado == 3 || resultado == 4) {
+				System.out.println("A palavra: "+palavraOriginal+" foi aceita");
+				System.out.println("Estado final: "+resultado);
+			}else {
+				System.out.println("Palavra não aceita!");
+			}
+			System.out.println("\ncontinuar?");
+			System.out.println("__________________________");
+			
+			resposta = input.nextLine();
+			if (resposta.equals("n")) {
+				System.out.println("Programa finalizado!");
+				break;
+			}
+		}
+		
+	}
+		
+}
---
+++ src/Valida.java
@@ -0,0 +1,11 @@
+
+public class Valida {
+	
+	boolean valida(char[] caracter, int count) {
+		if(caracter[count] != 'a' && caracter[count] != 'b' && caracter[count] ==' ' && caracter.length == 0 ) {
+			return false;
+		}else 
+			return true;
+		
+	}
+}
