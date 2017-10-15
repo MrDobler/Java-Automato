5c40fdaa6328f1d355ac9d2502dfd5785fc52df3
Lucas <lucasportojpa@gmail.com>
2017-10-15 19:27:42 -0200

    Classe valida alterada, E lógico trocado por Ou lógico

--- src/Valida.java
+++ src/Valida.java
@@ -3,7 +3,7 @@ public class Valida {
 	
 	boolean valida(char[] caracter, int count) 
 	{
-		if(caracter[count] != 'a' && caracter[count] != 'b' && caracter[count] ==' ') {
+		if(caracter[count] != 'a' && caracter[count] != 'b' || caracter[count] ==' ') {
 			return false;
 		}else {
 			return true;			
