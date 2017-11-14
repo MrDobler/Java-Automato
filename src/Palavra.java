4fa5829c3f01b9d71f58f9d793a4f364042fbdd7
Lucas Porto <lucasportojpa@gmail.com>
2017-11-14 00:09:10 -0200

    Alteração no código, melhor aproveitamento da linguagem

---
+++ src/Palavra.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Palavra
+{
+
+	public static void main(String[] args) 
+	{
+		String loop="sim";
+		String palavra;
+		char[] letras;
+		while(!loop.contains("nao") ) 
+		{
+			
+			@SuppressWarnings("resource")
+			Scanner input = new Scanner(System.in);
+		
+			System.out.println("informe a palavra a ser validada: ");
+			palavra = input.nextLine();
+			letras = palavra.toCharArray();
+			Valida valida = new Valida();
+		
+			valida.validacao(letras);
+		
+			System.out.println("A palavra: \""+palavra+"\""+valida.getResultado());
+			System.out.println("Estado final: q"+valida.getEstado());
+		
+			System.out.println("\nContinuar? sim ou nao");
+			loop = input.nextLine();
+	
+		}
+	}
+
+}
---
+++ src/Valida.java
@@ -0,0 +1,109 @@
+
+public class Valida 
+{
+	int estado = 0;
+	boolean palavraAceita = true;
+	String resultado;
+	
+	void validacao(char[] letras) 
+	{
+		
+	
+
+	for (int i = 0; i < letras.length; i++) 
+	{
+		// Q0
+		switch (estado) {
+
+			case 0:
+				if (letras[i] == 'a')
+					estado = 2;
+				else if (letras[i] == 'b')
+					estado = 1;
+				else {
+					palavraAceita = false;
+					i = letras.length;
+				}
+
+				break;
+
+			// Q1
+			case 1:
+				if (letras[i] == 'a')
+					estado = 1;
+				else if (letras[i] == 'b')
+					estado = 0;
+				else {
+					palavraAceita = false;
+					i = letras.length;
+				}
+				
+				break;
+			// Q2
+			case 2:
+				if (letras[i] == 'a')
+					estado = 4;
+				else if (letras[i] == 'b')
+					estado = 3;
+				else {
+					palavraAceita = false;
+					i = letras.length;
+				}
+				
+				break;
+			// Q3
+			case 3:
+				if (letras[i] == 'a')
+					estado = 2;
+				else if (letras[i] == 'b')
+					estado = 5;
+				else {
+					palavraAceita = false;
+					i = letras.length;
+				}
+				
+				break;
+			// Q4
+			case 4:
+				if (letras[i] == 'a')
+					estado = 5;
+				else if (letras[i] == 'b')
+					estado = 2;
+				else {
+					palavraAceita = false;
+					i = letras.length;
+				}
+				
+				break;
+			// Q5
+			case 5:
+				if (letras[i] == 'a')
+					estado = 3;
+				else if (letras[i] == 'b')
+					estado = 4;
+				else {
+					palavraAceita = false;
+					i = letras.length;
+				}
+				
+				break;
+
+			}
+		}
+	
+		if (palavraAceita && (estado == 3  || estado == 4))
+			resultado = " foi aceita pelo automato";	
+		else
+			resultado =" não foi aceita pelo autômato";	
+	}
+	
+	
+	int getEstado() {
+		return estado;
+	}
+	
+	String getResultado() {
+		return resultado;
+	}
+	
+}
